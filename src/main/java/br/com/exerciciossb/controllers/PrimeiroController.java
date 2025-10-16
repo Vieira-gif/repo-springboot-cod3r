38bf49ababd6a5df5a1034e3832f5f5e2431dbc6
João Vieira <mutantinotrue@outlook.com>
2025-10-15 21:05:41 -0300

    Testando Mapping

---
+++ src/main/java/br/com/exerciciossb/controllers/PrimeiroController.java
@@ -0,0 +1,19 @@
+package br.com.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeiroController {
+
+    @GetMapping(path = {"/ola", "/saudacao" })
+    public String ola() {
+        return "Olá Spring Boot GET";
+    }
+
+    @PostMapping(path = { "/saudacao" })
+    public String saudacao() {
+        return "Olá Spring Boot POST";
+    }
+}
