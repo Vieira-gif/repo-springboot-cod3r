1e8403bdce37cc26c5115fb9f96a31c2536bc8a1
João Vieira <mutantinotrue@outlook.com>
2025-10-15 21:06:07 -0300

    Iniciando aplicação

---
+++ src/main/java/br/com/exerciciossb/ExercicioSbApplication.java
@@ -0,0 +1,11 @@
+package br.com.exerciciossb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioSbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExercicioSbApplication.class, args);
+    }
+}
