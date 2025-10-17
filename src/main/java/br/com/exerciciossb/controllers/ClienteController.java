379c74dffba21556b3c5f9ef8be17b7a6174d09f
João Vieira <mutantinotrue@outlook.com>
2025-10-16 21:41:02 -0300

    Retornando JSON por GET

--- src/main/java/br/com/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/exerciciossb/controllers/ClienteController.java
@@ -1,9 +1,8 @@
 package br.com.exerciciossb.controllers;
 
 import br.com.exerciciossb.models.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clientes" )
@@ -12,5 +11,16 @@ public class ClienteController {
     @GetMapping("/qualquer")
     Cliente obterCliente() {
         return new Cliente("JTS", 22, "123.456.789.-00");
+    }
+
+    @GetMapping("/{id}")
+    Cliente obterClientePorId(@PathVariable int id) {
+        return new Cliente("JTS 2", id, "123.456.789.-00");
+    }
+
+    @GetMapping
+    Cliente obterClientePorNomeQuery(
+            @RequestParam(name = "id", defaultValue = "2") int id) {
+        return new Cliente("JTS 3", id, "123.456.789.-00");
     }
 }
